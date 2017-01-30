2cd5e8c71b704d765cd7255dafb1103bb0e852a3
Daniel Osvath Londono <o.dani8@yahoo.com>
2017-01-29 20:54:37 -0500

    Reading in data works

--- readData.py
+++ readData.py
@@ -9,34 +9,87 @@
 """
 
 import pandas
+import matplotlib.pyplot as plt
+
+#amount of user data to read.
+NUMBER_OF_USERS = 100
+
+#amount of data for each user.
+AMOUNT_MONTHS = 24
 
 
-def readUsers(amount):
-    """
-        Read the data for a number of users,
-        assuming transaction data is provided, within range
-        and named user-#.csv
 
-        :param amount - amount of user data to read
-    """
-
-    for i in range(0,amount):
-        filename = "transaction-data/user-" + str(i) + ".csv"
-        loadUserData(filename)
-
-
-def loadUserData(filename):
+def loadUserData(current):
     """
         Load the data for a user.
     """
+    filename = "transaction-data/user-" + current + ".csv"
+
     user = pandas.read_csv(filename)
-    print(user.columns)
+
+    return user
 
 
+
+def avgSpending(user):
+    """
+        Get the average spending of a user, annually and monthly.
+
+        :param user: user data instance.
+    """
+
+    sum = 0
+
+    user[' Date'] = pandas.to_datetime(user[' Date'], format="%m/%d/%Y")
+
+    start = "1/1/2013"
+    end = "12/31/2014"
+
+    years = user[(user[' Date'] >= pandas.to_datetime(start, format='%m/%d/%Y'))
+    & (user[' Date'] <= pandas.to_datetime(end, format='%m/%d/%Y'))]
+
+    amounts = years[' Amount'].values
+
+    # transactionCount = amounts.shape[0]
+
+    for cost in amounts:
+        sum += abs(cost)
+
+
+    monthly = (sum / AMOUNT_MONTHS)
+    annually  = sum / 2
+
+    return [monthly, annually]
+
+
+# def recordData():
+#     """
+#         Assumes table headers already present in file results.csv
+#     """
 
 
 def main():
-    readUsers(5)
+    """
+        Read the data for a number of users, and record stats.
+        (Assuming transaction data is provided, within range
+        and named user-#.csv)
+
+        :param amount: amount of user data to read
+    """
+
+    for current in range(0,1):
+
+        currentUser = loadUserData(str(current))
+
+        avgMonthly = avgSpending(currentUser)[0]
+        avgAnnual = avgSpending(currentUser)[1]
+
+        incomeEstimate = avgAnnual / 0.85
+
+        #recordData(id,incomeEstimate,)
+
+
+
 
 if __name__ == '__main__':
     main()
